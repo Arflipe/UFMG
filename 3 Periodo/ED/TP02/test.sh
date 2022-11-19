55873840cd7fdcda3a9a16e99caa6a0f2bc919f2
Arflipe <flipeara@gmail.com>
2022-11-19 03:28:29 -0300

    Test file edit

--- 3 Periodo/ED/TP02/test.sh
+++ 3 Periodo/ED/TP02/test.sh
@@ -1,15 +1,19 @@
 make clean
 make
 
-./bin/run.out -v 1 -s 10 -i Inputs/Input.txt -o Outputs/RQuickSort
+./bin/run.out -v 1 -s 1 -i inputs/input.txt
 
-./bin/run.out -v 2 -s 10 -i Inputs/Input.txt -o Outputs/M3QuickSort -k 3
-./bin/run.out -v 2 -s 10 -i Inputs/Input.txt -o Outputs/M5QuickSort -k 5
-./bin/run.out -v 2 -s 10 -i Inputs/Input.txt -o Outputs/M7QuickSort -k 7
+./bin/run.out -v 2 -s 1 -i inputs/input.txt -k 3
+./bin/run.out -v 2 -s 1 -i inputs/input.txt -k 5
+./bin/run.out -v 2 -s 1 -i inputs/input.txt -k 7
 
-./bin/run.out -v 3 -s 10 -i Inputs/Input.txt -o Outputs/S10QuickSort -m 10
-./bin/run.out -v 3 -s 10 -i Inputs/Input.txt -o Outputs/S100QuickSort -m 100
+./bin/run.out -v 3 -s 1 -i inputs/input.txt -m 10
+./bin/run.out -v 3 -s 1 -i inputs/input.txt -m 100
 
-./bin/run.out -v 4 -s 10 -i Inputs/Input.txt -o Outputs/IQuickSort
+./bin/run.out -v 4 -s 1 -i inputs/input.txt
 
-./bin/run.out -v 5 -s 10 -i Inputs/Input.txt -o Outputs/SSQuickSort
+./bin/run.out -v 5 -s 1 -i inputs/input.txt
+
+./bin/run.out -v 6 -s 1 -i inputs/input.txt
+
+./bin/run.out -v 7 -s 1 -i inputs/input.txt
