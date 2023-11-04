24c2fec242a80babb0bfccc242eabcf4ae126ead
Arflipe <flipeara@gmail.com>
2023-11-04 03:42:10 -0300

    Rest of the instances, solutions and test scripts

---
+++ 5 Periodo/PO/TP01/Ex2/test.sh
@@ -0,0 +1,7 @@
+clear
+julia tp1_2021031920.jl INST1_PB2.txt
+head -n 1 SOL1_PB2.txt
+julia tp1_2021031920.jl INST2_PB2.txt
+head -n 1 SOL2_PB2.txt
+julia tp1_2021031920.jl INST3_PB2.txt
+head -n 1 SOL3_PB2.txt
---
+++ 5 Periodo/PO/TP01/Ex4/test.sh
@@ -0,0 +1,7 @@
+clear
+julia tp1_2021031920.jl INST1_PB4.txt
+head -n 1 SOL1_PB4.txt
+julia tp1_2021031920.jl INST2_PB4.txt
+head -n 1 SOL2_PB4.txt
+julia tp1_2021031920.jl INST3_PB4.txt
+head -n 1 SOL3_PB4.txt
