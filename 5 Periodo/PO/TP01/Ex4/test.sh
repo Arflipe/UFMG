d9510e1bf3c76efe0a9bc3f1a1ccdc99a7d37391
Arflipe <flipeara@gmail.com>
2023-11-05 05:08:25 -0300

    Updated Test scripts

--- 5 Periodo/PO/TP01/Ex4/test.sh
+++ 5 Periodo/PO/TP01/Ex4/test.sh
@@ -1,7 +1,6 @@
-clear
-julia tp1_2021031920.jl INST1_PB4.txt
+/home/flipe/julia-1.8.1/bin/julia tp1_2021031920.jl INST1_PB4.txt
 head -n 1 SOL1_PB4.txt
-julia tp1_2021031920.jl INST2_PB4.txt
+/home/flipe/julia-1.8.1/bin/julia tp1_2021031920.jl INST2_PB4.txt
 head -n 1 SOL2_PB4.txt
-julia tp1_2021031920.jl INST3_PB4.txt
+/home/flipe/julia-1.8.1/bin/julia tp1_2021031920.jl INST3_PB4.txt
 head -n 1 SOL3_PB4.txt
---
+++ 5 Periodo/PO/TP01/test.sh
@@ -0,0 +1,16 @@
+clear
+cd Ex1
+./test.sh
+cd ..
+cd Ex2
+./test.sh
+cd ..
+cd Ex3
+./test.sh
+cd ..
+cd Ex4
+./test.sh
+cd ..
+cd Ex5
+./test.sh
+cd ..
